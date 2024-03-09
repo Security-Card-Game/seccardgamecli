b1ef5fba1d045c55960bd1601fb8b0360e69b0e7
maschmi <maschmi@maschmi.net>
2024-03-09 14:43:25 +0100

    decreases attack duration when round proceeds

--- game_lib/src/world/board.rs
+++ game_lib/src/world/board.rs
@@ -3,6 +3,9 @@ use std::rc::Rc;
 
 use uuid::Uuid;
 
+use crate::cards::properties::duration::Duration;
+use crate::cards::types::attack::AttackCard;
+use crate::cards::types::card_model::Card;
 use crate::world::deck::{CardRc, Deck};
 use crate::world::resources::Resources;
 
@@ -31,7 +34,7 @@ impl CurrentBoard {
         let current_resources = &self.current_resources;
         let deck = &self.deck;
         let cards = &self.deck.board;
-        let mut open_cards = &mut self.open_cards.clone();
+        let mut open_cards = &mut update_open_cards(self.open_cards.clone());
         let (drawn_card, rest) = cards.split_at(1);
         let card_ref = Rc::new(drawn_card[0].clone());
 
@@ -67,5 +70,40 @@ impl CurrentBoard {
             deck: self.deck.clone(),
             turns_remaining: self.turns_remaining,
         }
+    }
+}
+
+fn update_open_cards(input: HashMap<Uuid, CardRc>) -> HashMap<Uuid, CardRc> {
+    let mut result = HashMap::new();
+    for (key, card) in input.iter() {
+        let card_to_insert = match &**card {
+            Card::Attack(ac) => update_attack_card(card, ac),
+            Card::Event(_) | Card::Oopsie(_) | Card::Lucky(_) => Some(card.clone()),
+        };
+
+        match card_to_insert {
+            None => {}
+            Some(c) => {
+                result.insert(key.clone(), c);
+            }
+        }
+    }
+    result
+}
+
+fn update_attack_card(card: &CardRc, ac: &AttackCard) -> Option<Rc<Card>> {
+    let new_duration = ac.duration.decrease();
+    match new_duration {
+        Duration::Rounds(_) => {
+            let updated_card = Card::Attack(AttackCard {
+                title: ac.title.clone(),
+                description: ac.description.clone(),
+                effect: ac.effect.clone(),
+                duration: new_duration,
+            });
+            Some(Rc::new(updated_card))
+        }
+        Duration::UntilClosed => Some(card.clone()),
+        Duration::None => None,
     }
 }
