7fabef0517a344d2d7a0ad70b7b4e0346c7c760b
maschmi <maschmi@maschmi.net>
2024-06-16 21:16:30 +0200

    moves control panel to extra file to signal scope

--- gui/src/lib.rs
+++ gui/src/lib.rs
@@ -1,6 +1,26 @@
-pub use app::SecCardGameApp;
+use game_lib::world::game::Game;
 
 mod app;
 mod card_view_model;
 mod card_window;
 pub mod start;
+mod control_panel;
+
+pub struct SecCardGameApp {
+    game: Game,
+    input: Input,
+}
+
+enum Message {
+    Success(String),
+    Failure(String),
+    Warning(String),
+    None,
+}
+
+struct Input {
+    next_res: String,
+    pay_res: String,
+    message: Message,
+    multiplier: String,
+}
