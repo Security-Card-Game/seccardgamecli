c573838be39a7610ce1291db3d5223acb83fac02
maschmi <maschmi@maschmi.net>
2024-09-24 15:56:36 +0200

    using new input and message for pay resources

--- gui/src/controls/resource_control.rs
+++ gui/src/controls/resource_control.rs
@@ -9,7 +9,6 @@ use crate::controls::messageing::UpdateMessage;
 use crate::SecCardGameApp;
 
 impl SecCardGameApp {
-
     pub(crate) fn resource_control(&mut self, ui: &mut Ui) {
         ui.label("Resources");
         ui.add_space(5.0);
@@ -28,17 +27,18 @@ impl SecCardGameApp {
         let available = create_resource_label(&cloned_board, "available");
         ui.label(available);
 
-        let modifier = create_fix_modifier_label(self.game.get_current_fix_modifier(), &self.game.fix_multiplier);
+        let modifier = create_fix_modifier_label(
+            self.game.get_current_fix_modifier(),
+            &self.game.fix_multiplier,
+        );
         ui.label(modifier);
 
-        ui.horizontal(|ui| {
-            self.numeric_enter_component(
-                ui,
-                |game| &mut game.input.pay_res,
-                "Pay",
-                |value| UpdateMessage::PayResources(value)
-            );
-        });
+        self.numeric_enter_component(
+            ui,
+            |game| &mut game.input.pay_res,
+            "Pay",
+            |value| UpdateMessage::PayResources(value),
+        );
     }
 }
 
@@ -47,26 +47,23 @@ fn game_ended(ui: &mut Ui, board: &Board) {
     ui.label(available);
 }
 
-
 // helper function to create rich text for resource amount,
 // will be used in match arms for GameStatus
 fn create_resource_label(board: &Board, postfix: &str) -> RichText {
-    let resource_str = format!(
-        "{} {}", board.current_resources.value(), postfix
-    );
-    RichText::new(resource_str)
-        .strong()
+    let resource_str = format!("{} {}", board.current_resources.value(), postfix);
+    RichText::new(resource_str).strong()
 }
 
 // function to create fix modifier label
-fn create_fix_modifier_label(fix_modifier: Option<FixModifier>, multiplier: &ResourceFixMultiplier) -> String {
+fn create_fix_modifier_label(
+    fix_modifier: Option<FixModifier>,
+    multiplier: &ResourceFixMultiplier,
+) -> String {
     match fix_modifier {
         None => "No cost modifier active!".to_string(),
-        Some(m) => {
-            match m {
-                FixModifier::Increase(r) => format!("Next fix is increased by: {}", r * multiplier),
-                FixModifier::Decrease(r) => format!("Next fix is decreased by: {}", r * multiplier),
-            }
-        }
+        Some(m) => match m {
+            FixModifier::Increase(r) => format!("Next fix is increased by: {}", r * multiplier),
+            FixModifier::Decrease(r) => format!("Next fix is decreased by: {}", r * multiplier),
+        },
     }
 }
