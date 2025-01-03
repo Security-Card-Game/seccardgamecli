4202abb4fc5ec830303a4da012a50e10e31eb9c1
maschmi <maschmi@maschmi.net>
2025-01-03 17:28:48 +0100

    limits reputation to 100

--- game_lib/src/world/reputation.rs
+++ game_lib/src/world/reputation.rs
@@ -1,13 +1,21 @@
 use std::fmt::Display;
 use std::ops::{Add, Sub};
 
-#[derive(Debug, Clone, PartialEq, PartialOrd, Default)]
-pub struct Reputation(usize);
+const MAX_VALUE: u8 = 100;
 
+#[derive(Debug, Clone, PartialEq, PartialOrd, Default)]
+pub struct Reputation(u8);
 
 impl Reputation {
-    pub fn new(rep: usize) -> Self { Reputation(rep) }
-    pub fn value(&self) -> &usize { &self.0 }
+    pub fn new(rep: u8) -> Self {
+        if rep > MAX_VALUE {
+            panic!("Reputation can't be higher than {}", MAX_VALUE);
+        }
+        Reputation(rep)
+    }
+    pub fn value(&self) -> &u8 {
+        &self.0
+    }
 }
 
 impl Add for &Reputation {
@@ -15,7 +23,11 @@ impl Add for &Reputation {
 
     fn add(self, rhs: Self) -> Self::Output {
         let sum = self.0 + rhs.0;
-        Reputation(sum)
+        if sum > MAX_VALUE {
+            Reputation::new(100)
+        } else {
+            Reputation::new(sum)
+        }
     }
 }
 
@@ -24,7 +36,7 @@ impl Sub for &Reputation {
 
     fn sub(self, rhs: Self) -> Self::Output {
         let sum = self.0 - rhs.0;
-        Reputation(sum)
+        Reputation::new(sum)
     }
 }
 
@@ -34,18 +46,22 @@ impl Display for Reputation {
     }
 }
 
-
-
 #[cfg(test)]
 mod tests {
-
     use super::*;
 
     #[test]
-    pub fn create_reputation() {
-        let rep = Reputation::new(10);
-        assert_eq!(rep.value(), &10);
+    pub fn create_reputation_max_value() {
+        let rep = Reputation::new(100);
+        assert_eq!(rep.value(), &100);
     }
+
+    #[test]
+    #[should_panic]
+    pub fn cannot_create_reputation_above_100() {
+        Reputation::new(101);
+    }
+
 
     #[test]
     pub fn add_10_to_20_should_return_30() {
@@ -57,6 +73,18 @@ mod tests {
 
         assert_eq!(result, expected);
     }
+
+    #[test]
+    pub fn add_10_to_99_should_return_100() {
+        let input = Reputation::new(99);
+        let add_sum = Reputation::new(10);
+        let expected = Reputation::new(100);
+
+        let result = &input + &add_sum;
+
+        assert_eq!(result, expected);
+    }
+
 
     #[test]
     pub fn sub_10_from_30_should_return_20() {
@@ -83,5 +111,4 @@ mod tests {
         let rep = Reputation::new(10);
         assert_eq!(format!("{}", rep), "10 reputation");
     }
-
 }
